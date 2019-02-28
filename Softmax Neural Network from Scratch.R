73e4cbb1c97bf402b9ab9845bc7408b773d6025b
Brian-Q <bri.quach90@gmail.com>
2019-02-27 16:59:32 -0800

    Update Softmax Neural Network from Scratch.R
    Credited David Selby for original functions of feedforward, backpropagate, and train - all of which I have modified for multiple classification.
    
    Fixed some typos.

--- Softmax Neural Network from Scratch.R
+++ Softmax Neural Network from Scratch.R
@@ -1,5 +1,3 @@
-library(ggplot2)
-
 ### SOFTMAX IMPLEMENTATION
 
 #Iris data set including 4 features and 3 different target
@@ -25,6 +23,8 @@ dlyrelu <- function(x) {
   ifelse(x>=0, 1, 0.01)
 }
 
+#credit goes to David Selby for feedforward, backpropagate, and train (source: https://selbydavid.com/2018/01/09/neural-network/)
+#I have made significant modifications to these functions to expand from binary classification to multiple classification.
 feedforward <- function(x, w1, w2) {
   z1 <- cbind(1, x) %*% w1
   h <- lyrelu(z1)
@@ -59,8 +59,11 @@ train <- function(x, y, hidden, learn_rate, iterations) {
   list(output = ff$output, w1 = w1, w2 = w2)
 }
 
-#Training the Softmax neural network with 10 hidden layers over 100,000 iterations.
-nnet5 <- train(x[,1:2], y, hidden=10, learn_rate =1e-4, iterations=1e5) 
+#Training the Softmax neural network with 10 hidden layers over 100,000 iterations. 
+#Used only two features here for now, for visualization purposes. 
+nnet10 <- train(x[,1:2], y, hidden=10, learn_rate =1e-4, iterations=1e5) 
 
-#Model accuracy
-mean(apply(nnet5$output, 1, which.max) == apply(y, 1, which.max))
+#Model accuracy (roughly 83% on the entire data set with 2 features)
+mean(apply(nnet10$output, 1, which.max) == apply(y, 1, which.max))
+
+#We can do better by including all 4 features rather than just two.
